108cff5735405d0eea696a59e4a501845a7c22f7
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-04 14:04:06 +0000

    Automatically backed up by Learn

--- db/migrate/20200504140053_create_dogs.rb
+++ db/migrate/20200504140053_create_dogs.rb
@@ -1,7 +1,14 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def change
-      create_table :dogs do |t|
-        t.string :name
-        t.breed :name
-  end
+
+  def up 
+    create_table :dogs do |t|
+      t.string :name
+      t.breed :name
+    end 
+  end 
+
+  def down 
+    drop_table :dogs
+  end 
+  
 end
